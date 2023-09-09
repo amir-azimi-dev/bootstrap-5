88124fa17762ba9da9ed4602e0671654f8b7d859
Amir Azimi <azymy3354@gmail.com>
2023-09-09 13:00:07 +0330

    add: sidebar section

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data("toggleSidebar", () => ({
+        open: (window.innerWidth >= 992),
+
+        toggle() {
+            this.open = !this.open;
+        }
+    }));
+});
