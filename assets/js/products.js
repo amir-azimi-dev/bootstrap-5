af7553af50dd7424864e94366a373810105f0d83
Amir Azimi <azymy3354@gmail.com>
2023-09-10 18:36:59 +0330

    add: product.html file

---
+++ assets/js/products.js
@@ -0,0 +1,10 @@
+// alpine
+document.addEventListener('alpine:init', () => {
+    Alpine.data("toggleSidebar", () => ({
+        open: (window.innerWidth >= 992),
+
+        toggle() {
+            this.open = !this.open;
+        }
+    }));
+});
