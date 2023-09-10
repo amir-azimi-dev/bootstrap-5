b53864ef34ab3048d25f535f7f8f5fad83c057bd
Amir Azimi <azymy3354@gmail.com>
2023-09-10 17:00:18 +0330

    add: users and doughnut-chart  sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,99 +13,70 @@ document.addEventListener('alpine:init', () => {
 new PureCounter();
 
 
-// amChart 1
-am5.ready(function () {
-    let root = am5.Root.new("chart-div");
-    root._logo.dispose();
+// bar chart
+const chartbar = document.getElementById('chart-bar');
 
-    root.setThemes([
-        am5themes_Animated.new(root)
-    ]);
+new Chart(chartbar, {
+    type: 'bar',
+    data: {
+        labels: ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور",],
 
-    let chart = root.container.children.push(am5xy.XYChart.new(root, {
-        panX: true,
-        panY: true,
-        wheelX: "panX",
-        wheelY: "zoomX",
-        pinchZoomX: true
-    }));
+        datasets: [{
+            data: [300, 150, 167, 280, 234, 325],
+            backgroundColor: [
+                '#9694ff',
+                '#5ddab4',
+                '#ff7976',
+                '#57caeb'
+            ],
+            hoverOffset: 4
+        }]
+    },
 
-
-    let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-    cursor.lineY.set("visible", false);
-
-
-    let xRenderer = am5xy.AxisRendererX.new(root, {minGridDistance: 30});
-    xRenderer.labels.template.setAll({
-        rotation: -90,
-        centerY: am5.p50,
-        centerX: am5.p100,
-        paddingRight: 15,
-    });
-
-    xRenderer.grid.template.setAll({
-        location: 1
-    })
-
-    let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-        maxDeviation: 0.3,
-        categoryField: "month",
-        renderer: xRenderer,
-        tooltip: am5.Tooltip.new(root, {})
-    }));
-
-    let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-        maxDeviation: 0.3,
-        renderer: am5xy.AxisRendererY.new(root, {
-            strokeOpacity: 0.1
-        })
-    }));
-
-    let series = chart.series.push(am5xy.ColumnSeries.new(root, {
-        name: "Series 1",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value",
-        sequencedInterpolation: true,
-        categoryXField: "month",
-        tooltip: am5.Tooltip.new(root, {
-            labelText: "{valueY}"
-        })
-    }));
-
-    series.columns.template.setAll({cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0});
-    series.columns.template.adapters.add("fill", function (fill, target) {
-        return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    series.columns.template.adapters.add("stroke", function (stroke, target) {
-        return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    let data = [{
-        month: "دی",
-        value: 2025
-    }, {
-        month: "بهمن",
-        value: 1114
-    }, {
-        month: "اسفند",
-        value: 1809
-    }, {
-        month: "فروردین",
-        value: 1122
-    }, {
-        month: "اردیبهشت",
-        value: 1322
-    }, {
-        month: "خرداد",
-        value: 1882
-    }];
-
-    xAxis.data.setAll(data);
-    series.data.setAll(data);
-
-    series.appear(1000);
-    chart.appear(1000, 100);
-
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
 });
+
+// doughnut chart
+const ctx = document.getElementById('doughnut-chart-div');
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'پرداختی‌های موفق',
+            'پرداختی‌های ناموفق',
+        ],
+
+        datasets: [{
+            data: [300, 50],
+            backgroundColor: [
+                '#9694ff',
+                '#57caeb',
+            ],
+            hoverOffset: 4
+        }]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                rtl: true,
+                position: "bottom",
+                labels: {
+                    font: {
+                        family: "IranYekan",
+                        weight: "bold",
+                        size: 15
+                    }
+                }
+            }
+        }
+    }
+});
+
